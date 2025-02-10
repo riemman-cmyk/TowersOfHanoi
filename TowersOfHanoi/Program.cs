7704fb8f22801543f4e94806132d6aad76f5e9a4
riemman-cmyk <nika.nika.khundadze@gmail.com>
2025-02-10 19:30:22 +0400

    comment cancelation

--- TowersOfHanoi/Program.cs
+++ TowersOfHanoi/Program.cs
@@ -7,11 +7,8 @@
             Console.Write("number of disks: ");
             int n = Convert.ToInt32(Console.ReadLine());
             place_disk(n, 1, 3);
-            //sawyisi da samizne rodebi aris meore da mesame parametri
         }
-        //s aris sawyisi rod, xolo e aris saboloo rod romelzec struqtura gadagvaq
-        //1-dan 3is chatvlit
-        static void place_disk(int n, int s, int e)
+        static void place_disk(int n, int s, int e)  
         {
             if (n == 1)
             {
@@ -19,13 +16,8 @@
             }
             else
             {
-                //k aris is rod romelzec n-1 diskiani struqtura gadagvaq
-                // 6 raxan yvela matagnis indexis jami 6 aris
-                //mashin sawyisis da saboloos garda mesame iqneba
-                //samives jams minus mati maji
                 int k = 6 - e - s;
                 place_disk(n - 1, s, k);
-                //gamogvaqs shesabamisi diskebis gadanacvleba
                 print_placements(n, s, e);
                 place_disk(n - 1, k, e);
             }
